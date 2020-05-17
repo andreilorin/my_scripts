ae837ed7a116e2cf9a077a4e5c612431d2989dd3
andreilorin <andreilorin@gmail.com>
2020-05-17 12:59:07 +0100

    Add more info to stats

--- test_scripts/stats.sh
+++ test_scripts/stats.sh
@@ -1,1 +1,12 @@
-free -h | awk '/^Mem:/ { print "Memory" $3 "/" $2}'
+#!/bin/bash
+memory=`free -h | awk '/^Mem:/ { print $3 "/" $2}'`
+
+processes=`ps axch -o cmd:15,%mem --sort=-%mem | head`
+
+echo "Memory $memory"
+echo "$processes"
+
+# add this to a run commands option in dmenu(my_script)
+# use notify send, entr
+
+# ls folder | entr notify-send "Script Modified"
