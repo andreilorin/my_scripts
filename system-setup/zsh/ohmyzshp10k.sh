6be5570804ed18a28b161a7e56eec9c75b914ef1
andreilorin <andreilorin@gmail.com>
2020-09-21 14:55:09 +0100

    Use sudo in script where necesary;Reorder commands

--- system-setup/zsh/ohmyzshp10k.sh
+++ system-setup/zsh/ohmyzshp10k.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-apt install curl -y
-apt install git -y
-apt install zsh -y
-apt install neovim -y
-apt install ttf-dejavu -y
+sudo apt install curl -y
+sudo apt install git -y
+sudo apt install zsh -y
+sudo apt install neovim -y
+sudo apt install ttf-dejavu -y
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -n)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
-sudo chown $USER:$USER MesloLGS*
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -n)"
 
 # https://www.youtube.com/watch?v=A6xWiqOpulI
 # https://www.youtube.com/watch?v=dg1tK50UVGI
