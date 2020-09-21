1c8d610a538b7a904148c5c5b148a6c96abae8ba
andreilorin <andreilorin@gmail.com>
2020-09-21 14:21:56 +0100

    Add system setup - oh my zsh powerlevel10k

---
+++ system-setup/ohmyzshp10k.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install neovim -y
+apt install git -y
+apt install zsh -y
+apt install ttf-dejavu -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -n)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+sudo chown $USER:$USER MesloLGS*
+
+# https://www.youtube.com/watch?v=A6xWiqOpulI
+# https://www.youtube.com/watch?v=dg1tK50UVGI
+# https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
